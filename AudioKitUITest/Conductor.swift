129bebc33fe130618c649174736137aa83f9432e
Matt Pfeiffer <mattpfeiffer54@gmail.com>
2021-01-01 19:01:52 -0600

    Additional code cleanup

--- AudioKitUITest/Conductor.swift
+++ AudioKitUITest/Conductor.swift
@@ -10,9 +10,6 @@ import Foundation
 
 class Conductor : ObservableObject{
     
-    /// Single shared data model
-    static let shared = Conductor()
-    
     /// Audio engine instance
     let engine = AudioEngine()
         
