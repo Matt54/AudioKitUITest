28d0edbfc2a3c2615395315687ae40e821937ea5
Matt Pfeiffer <mattpfeiffer54@gmail.com>
2021-01-01 18:35:48 -0600

    Initial Commit

---
+++ AudioKitUITest/AudioKitUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioKitUITestApp.swift
+//  AudioKitUITest
+//
+//  Created by Matt Pfeiffer on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct AudioKitUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
